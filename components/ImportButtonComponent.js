cbed640d9153268a25477d2bc08bba2613f5b0de
Cheuk Hin (Kelvin) Lau <clau128@my.bcit.ca>
2017-12-02 20:17:50 -0800

    buttons are now side by side and at the bottom of the screen

--- components/ImportButtonComponent.js
+++ components/ImportButtonComponent.js
@@ -1,26 +1,17 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import {Text, View, StyleSheet } from 'react-native';
-import { Button } from 'react-native-elements';
+import { SocialIcon } from 'react-native-elements';
 
 export default class ImportButtonComponent extends Component {
 
 	render(){
 		return(
 			<View>
-				<Button 
-					buttonStyle={styles.button} 
-					fontWeight='bold' 
-					backgroundColor='green' 
-					title='IMPORT' 
+				<SocialIcon 
+  					button type='instagram'					
+  					title='IMPORT' 
 				/>
 			</View>
 		);
 	}
 }
-const styles = StyleSheet.create({
-    button: {
-    	width: '50%',
-    	borderRadius:30
-    }
-});
